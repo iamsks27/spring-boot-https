ac67c1da403b599d5cd3f6dbeaad741c09c6c572
Shivam.Singh <shivam.singh@lavainternational.in>
2018-05-21 11:56:34 +0530

    created simple spring boot application with https support

--- src/main/java/com/https/example/HttpsApplication.java
+++ src/main/java/com/https/example/HttpsApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HttpsExampleApplication {
+public class HttpsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HttpsExampleApplication.class, args);
+		SpringApplication.run(HttpsApplication.class, args);
 	}
 }
